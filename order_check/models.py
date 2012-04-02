9d485b2593231f497bf031e8f9676a485b6eecd8
onuryavuz <d.onur.yavuz@gmail.com>
2012-04-02 22:57:43 +0200

    models.Orders is updated

--- order_check/models.py
+++ order_check/models.py
@@ -3,11 +3,20 @@ from django.contrib import admin
 
 # Create your models here.
 class Order(models.Model):
-    order_number = models.CharField(max_length=50)
-    barcode = models.CharField(max_length=50)
-    quantity = models.IntegerField(max_length=10)
+    id_sales_order_item = models.IntegerField(max_length=10)
+    order_nr = models.CharField(max_length=50)
+    size = models.IntegerField(max_length=11)
+    sku = models.CharField(max_length=255)
+    supplier_name = models.CharField(max_length=255)
+    sku_supplier_simple = models.CharField(max_length=50)
+    shipment_type = models.CharField(max_length=255)
+    buysheet_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    paid_price = models.DecimalField(max_digits=10,decimal_places=2)
+    cost = models.DecimalField(max_digits=10,decimal_places=2)
     def __unicode__(self):
-        return self.order_number
+        return self.order_nr
 
 class CrossStatus(models.Model):
     order_status   = models.CharField(max_length=30,default='unprocessed')

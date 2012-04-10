26740b12ef6c4d33e7f929ae768452886020b25f
Volkan <volkan.cirik@gmail.com>
2012-04-10 15:53:46 +0300

    login/register and authentication requirement are added.

--- order_check/models.py
+++ order_check/models.py
@@ -1,23 +1,78 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import UserManager
+from django.core import validators
+from django.forms import Form
+from django import forms
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
-class Order(models.Model):
-    id_sales_order_item = models.IntegerField(max_length=10)
-    order_nr = models.CharField(max_length=50)
-    size = models.IntegerField(max_length=11)
-    sku = models.CharField(max_length=255)
-    supplier_name = models.CharField(max_length=255)
-    sku_supplier_simple = models.CharField(max_length=50)
-    shipment_type = models.CharField(max_length=255)
-    buysheet_id = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
-    paid_price = models.DecimalField(max_digits=10,decimal_places=2)
-    cost = models.DecimalField(max_digits=10,decimal_places=2)
-    inbound_order_number = models.CharField(max_length=50)
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('B', 'Buyer'),
+        ('O', 'Operations'),
+        ('A', 'Admin'),
+        ('F','Fetcher')
+    )
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+    user = models.OneToOneField(User)
     def __unicode__(self):
-        return self.order_nr
+        return self.user.username
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        up = instance.get_profile()
+        up.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+
+
+
+
+class Order(models.Model):
+    id_sales_order_item = models.PositiveIntegerField(max_length=10,null=False,default=0)
+    id_sales_order = models.PositiveIntegerField(max_length=10,null=True)
+    order_nr = models.CharField(max_length=45,null=True)
+    size = models.IntegerField(max_length=11,null=True)
+    sku = models.CharField(max_length=255,null=True)
+    supplier_name = models.CharField(max_length=255,null=True)
+    sku_supplier_simple = models.CharField(max_length=50,null=True)
+    shipment_type = models.CharField(max_length=255,null=True)
+    barcode_ean = models.CharField(max_length=255,null=True)
+    sku_supplier_config = models.CharField(max_length=50,null=True)
+    buysheet_id = models.CharField(max_length=255,null=True)
+    name = models.CharField(max_length=255,null=True)
+    status = models.CharField(max_length=255,null=True)
+    id_sales_order_item_shipment = models.PositiveIntegerField(max_length=10,null=True)
+    tracking_url = models.CharField(max_length=255,null=True)
+    suborder_number = models.CharField(max_length=20,null=True)
+    paid_price = models.DecimalField(max_digits=10,decimal_places=2,null=False)
+    cost = models.DecimalField(max_digits=10,decimal_places=2, null=True)
+    shipping_name = models.CharField(max_length=511,null=True)
+    shipping_city = models.CharField(max_length=255,null=True)
+    billing_name = models.CharField(max_length=511,null=True)
+    billing_city = models.CharField(max_length=255,null=True)
+    customer_first_name = models.CharField(max_length=255,null=True)
+    customer_last_name = models.CharField(max_length=255,null=True)
+    amount_paid = models.DecimalField(max_digits=10,decimal_places=2, null=False,default=0)
+    tax_percent = models.DecimalField(max_digits=10,decimal_places=2, null=True)
+    original_unit_price = models.DecimalField(max_digits=10,decimal_places=2, null=True)
+    unit_price = models.DecimalField(max_digits=10,decimal_places=2, null=False)
+    tax_amount = models.DecimalField(max_digits=10,decimal_places=2, null=True)
+    phone = models.CharField(max_length=255,null=True)
+    coupon_money_value = models.DecimalField(max_digits=10,decimal_places=2, null=True)
+    coupon_percent = models.IntegerField(max_length=11,null=True)
+    address1 = models.CharField(max_length=255,null=True)
+    address2 = models.CharField(max_length=255,null=True)
+    billing_address = models.CharField(max_length=255,null=True)
+    billing_address2 = models.CharField(max_length=255,null=True)
+    order_date = models.DateTimeField(blank=True, auto_now_add=True)
+    def __unicode__(self):
+        return str(self.id_sales_order_item)
 
 class CrossStatus(models.Model):
     order_status   = models.CharField(max_length=30,default='unprocessed')
@@ -27,8 +82,12 @@ class CrossStatus(models.Model):
 
 class LastUpdate(models.Model):
     updated_on = models.DateTimeField(blank=False, auto_now_add=True)
-    cross_status = models.CharField(max_length=30)
+    cross_status = models.CharField(max_length=30,default='unprocessed')
     order_id = models.ForeignKey(Order)
+    user_id = models.ForeignKey(UserProfile)
+    def __unicode__(self):
+       return str(self.order_id)+' changed to '+self.cross_status+' on '+str(self.updated_on)+' by '+self.user_id.user.username
+
 
 class Admin:
    prepopulated_fields = {'slug': ('title',)}
@@ -37,5 +96,7 @@ class Admin:
        ('Date information', {'fields': ['body'],'classes': ['collapse']})
    ]
 
+admin.site.register(UserProfile)
 admin.site.register(Order)
 admin.site.register(CrossStatus)
+admin.site.register(LastUpdate)
--- order_check/views.py
+++ order_check/views.py
@@ -3,45 +3,108 @@
 
 from models import Order
 from models import CrossStatus
+from models import UserProfile
+from models import LastUpdate
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse
-
+from datetime import date
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.http import Http404, HttpResponse
+import datetime, random, sha
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from datetime import date
+from django.contrib.auth import authenticate, login
 
 
+def main(request):
+    return render_to_response('main.html', context_instance = RequestContext(request))
+
+class SupplierDetails:
+    item_count = 0
+    supplier_name = ""
+
+@login_required
 def order(request):
+    supNameDetail = ""
+    end_date = datetime.datetime.now()
+    start_date = end_date - datetime.timedelta(days=20)
 
-    if request.method == 'POST':
-        keyword =  request.POST["keyword"]
-        column_name=request.POST["column_name"]
-        allOrders= Order.objects.all()
-        if column_name == 'supplier_name':
-          orders_dict = allOrders.filter(supplier_name__contains=keyword)
-        orderList = list()
+    if "supname" in request.GET:
+       supNameDetail = request.GET["supname"]
+    try:
+            ordersAll = Order.objects.filter(order_date__range =[start_date,end_date] )
+            suppliers = list()
+            for anOrder in ordersAll:
+                    if suppliers.__contains__(anOrder.supplier_name):
+                        print("var amk")
+                    else:
+                        suppliers.append(anOrder.supplier_name)
+                    try:
+                        statusForAnOrder = CrossStatus.objects.get( pk = anOrder.id)
+                        anOrder.cross_status = statusForAnOrder.order_status
+                    except CrossStatus.DoesNotExist:
+                        statusForAnOrder = CrossStatus.objects.create(order_id = anOrder,order_status = 'Unprocessed')
+                        fetcher = UserProfile.objects.get( role = 'F')
+                        LastUpdate.objects.create(updated_on = datetime.datetime.now() , cross_status = 'Unprocessed', order_id = anOrder, user_id = fetcher )
+                        anOrder.cross_status = 'Unprocessed'
+                        anOrder.updated_on = datetime.datetime.now()
+                        anOrder.updated_by = fetcher.user.username
+                    
+            supplierIndex = 0
+            supplierDetailList = []
+            for aSupplier in suppliers:
+                ic = Order.objects.filter(supplier_name=suppliers.__getitem__(supplierIndex)).count()
+                sd = SupplierDetails()
+                sd.item_count = ic
+                sd.supplier_name = suppliers.__getitem__(supplierIndex)
+                supplierDetailList.append(sd)
+                supplierIndex = supplierIndex + 1
 
-        for anOrder in orders_dict:
-            orderList.append(anOrder)
-        return render_to_response('orders.html', {'orders' : orderList},context_instance = RequestContext(request) )
-    else:
-        try:
-            orders = Order.objects.all()
-        except Order.DoesNotExist:
-            raise Http404
+            filteredOrders = list()
+            for anOrder in ordersAll:
+                if anOrder.supplier_name ==  supNameDetail:
+                    filteredOrders.append(anOrder)
+                elif supNameDetail == "":
+                    filteredOrders.append(anOrder)
 
-        for anOrder in orders:
-            try:
-                statusForAnOrder = CrossStatus.objects.get( pk = anOrder.id)
-                anOrder.status = statusForAnOrder.order_status
-            except CrossStatus.DoesNotExist:
-                statusForAnOrder = CrossStatus.objects.create(order_id = anOrder,order_status = 'Unprocessed')
-                anOrder.status = 'Unprocessed'
-    return render_to_response('orders.html', {'orders' : orders},context_instance = RequestContext(request) )
 
+            if (request.GET.has_key('page')):
+                page = request.GET['page']
+            else:
+                page = 1
+            for anOrder in filteredOrders:
+                if anOrder.order_date == date.today:
+                    try:
+                        statusForAnOrder = CrossStatus.objects.get( pk = anOrder.id)
+                        anOrder.status = statusForAnOrder.order_status
+                    except CrossStatus.DoesNotExist:
+                        statusForAnOrder = CrossStatus.objects.create(order_id = anOrder,order_status = 'Unprocessed')
+                        anOrder.status = 'Unprocessed'
+
+                paginator = Paginator(filteredOrders, 43)
+                try:
+                    orders = paginator.page(page)
+                except (EmptyPage, InvalidPage):
+                    orders = paginator.page(paginator.num_pages)
+
+                if supNameDetail == "":
+                    supNameDetail = "All Suppliers"
+            return render_to_response('orders.html', {'orders' : orders,'supplierDetailList' : supplierDetailList,'supNameDetail':supNameDetail,'end_date':end_date,'start_date':start_date},context_instance = RequestContext(request))
+    except Order.DoesNotExist:
+        raise Http404
+#    return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+
+@login_required
 def updateOrder(request):
 
     orderIDs = list()
-    orderIDs = request.POST.getlist('orderChecked')
+    orderIDs = request.POST.getlist('`')
 
     for anOrderID in orderIDs:
         try:
@@ -76,3 +139,48 @@ def test(request):
 
 def tabletest(request):
     return render_to_response('tableTest.html',context_instance = RequestContext(request) )
+
+def registerUser(request):
+    if request.POST:
+        firstName = request.POST["firstName"]
+        lastName = request.POST["lastName"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        try:
+            User.objects.get(username = email.split('@')[0])
+        except User.DoesNotExist:
+            user = User.objects.create_user(email.split('@')[0], email, password)
+            user.first_name = firstName
+            user.last_name = lastName
+        else:
+            return render_to_response('main.html')
+
+        #profile = user.get_profile()
+        #profile.role = 2
+
+        user.save()
+        login(request, user)
+        return HttpResponseRedirect("/orders/")
+    
+    else:
+        return HttpResponseRedirect("/orders/")
+
+def loginUser(request):
+    if request.POST:
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = authenticate(username=email.split('@')[0], password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect("/orders/")
+            else:
+                print "Your account has been disabled!"
+        else:
+            print "Your username and password were incorrect."
+
+
+    else:
+            return HttpResponseRedirect("/orders/")

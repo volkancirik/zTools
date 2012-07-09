438d29268495f21bad65b4fbf98e3acc60860f05
Baris Bilgic <Baris@Baris-Ultrabook.(none)>
2012-07-09 12:37:56 +0300

    initialization

---
+++ cross_app/main/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^list_documents/$', 'cross_app.dms.views.list_documents'),
+    (r'^upload_document/$', 'cross_app.dms.views.upload_document'),
+    (r'^document_action/$', 'cross_app.dms.views.document_action'),
+    (r'^edit_row/$', 'cross_app.dms.views.edit_row'),
+)

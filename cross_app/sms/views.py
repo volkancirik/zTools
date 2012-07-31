a8566d60d721e73053321b0382d68d3bd8d08606
onur.yavuz <d.onur.yavuz@gmail.com>
2012-07-31 11:24:36 +0300

    solving issue # 129

--- cross_app/sms/views.py
+++ cross_app/sms/views.py
@@ -8,6 +8,7 @@ from cross_order.utils import check_permission
 from sms.helper import getTotalShipmentItemCount, generateShipmentString
 from sms.models import Supplier, CatalogSimple, CatalogSupplier, CatalogBrand, ShipmentItem, Shipment, ShipmentType, ShipmentStatus, SimpleStatus,SimpleShipmentTypeID, BrandStatus, SupplierStatus, CancellationReason
 import csv
+import xlwt
 
 @login_required
 @check_permission('Sms')
@@ -21,18 +22,18 @@ def list_catalog_simple(request):
 
         return render_response(request, 'sms/list_catalog_simple.html',
                 {
-                    'supplier':sup,
-                    'csList':csList,
-                    'supList':CatalogSupplier.objects.filter( status = SupplierStatus.ACTIVE).order_by('name'),
-                    'totalShipmentItemCount':getTotalShipmentItemCount(request)
-                })
+                'supplier':sup,
+                'csList':csList,
+                'supList':CatalogSupplier.objects.filter( status = SupplierStatus.ACTIVE).order_by('name'),
+                'totalShipmentItemCount':getTotalShipmentItemCount(request)
+            })
     else:
 
         return render_response(request, 'sms/list_catalog_simple.html',
                 {
-                    'supList':CatalogSupplier.objects.filter( status = SupplierStatus.ACTIVE).order_by('name'),
-                    'totalShipmentItemCount':getTotalShipmentItemCount(request)
-                })
+                'supList':CatalogSupplier.objects.filter( status = SupplierStatus.ACTIVE).order_by('name'),
+                'totalShipmentItemCount':getTotalShipmentItemCount(request)
+            })
 
 @login_required
 @check_permission('Sms')
@@ -85,18 +86,10 @@ def clone_shipment(request):
     sid = request.POST.get("sid")
     shipment = Shipment.objects.get(pk = sid)
 
-    try:
-        request.session.__delitem__("supplier")
-    except:
-        pass
-    try:
-        request.session.__delitem__("siList")
-    except :
-        pass
-    try:
-        request.session.__delitem__("shipment")
-    except :
-        pass
+
+    request.session.__delitem__("supplier")
+    request.session.__delitem__("siList")
+    request.session.__delitem__("shipment")
 
     request.session["supplier"] = shipment.supplier.pk
     siListQuerySet = ShipmentItem.objects.filter( shipment = shipment)
@@ -109,7 +102,6 @@ def clone_shipment(request):
     totalCount = getTotalShipmentItemCount(request)
     json_models = simplejson.dumps(totalCount)
     return HttpResponse(json_models, mimetype='application/json; charset=utf8')
-
 
 @login_required
 @check_permission('Sms')
@@ -151,20 +143,18 @@ def check_basket(request):
     json_models = simplejson.dumps(result)
     return HttpResponse(json_models, mimetype='application/json; charset=utf8')
 
-
-
 @login_required
 @check_permission('Sms')
 def view_basket(request):
     shipment = request.session.get("shipment",None)
     siList = request.session.get("siList",[])
     return render_response(request, 'sms/view_basket.html',
-                {
-                    'shipment':shipment,
-                    'siList':siList,
-                    'shipmentTypeList':ShipmentType.objects.all().order_by("order"),
-                    'totalShipmentItemCount':getTotalShipmentItemCount(request)
-                })
+            {
+            'shipment':shipment,
+            'siList':siList,
+            'shipmentTypeList':ShipmentType.objects.all().order_by("order"),
+            'totalShipmentItemCount':getTotalShipmentItemCount(request)
+        })
 
 @login_required
 @check_permission('Sms')
@@ -225,7 +215,7 @@ def create_shipment(request):
 
         request.session.__delitem__("shipment")
         request.session.__delitem__("siList")
-    
+
         return redirect('/sms/list_shipment/')
     else:
         return redirect('/sms/view_basket/')
@@ -235,10 +225,10 @@ def create_shipment(request):
 def list_shipment(request):
     shipmentList = Shipment.objects.all().order_by("-create_date")
     return render_response(request, 'sms/list_shipment.html',
-                {
-                    'shipmentList':shipmentList,
-                    'totalShipmentItemCount':getTotalShipmentItemCount(request),
-                })
+            {
+            'shipmentList':shipmentList,
+            'totalShipmentItemCount':getTotalShipmentItemCount(request),
+            })
 
 @login_required
 @check_permission('Sms')
@@ -251,12 +241,12 @@ def view_shipment(request):
     siList = ShipmentItem.objects.filter(shipment=shipment)
 
     return render_response(request, 'sms/view_shipment.html',
-                {
-                    'shipment':shipment,
-                    'siList':siList,
-                    'totalShipmentItemCount':getTotalShipmentItemCount(request),
-                    'cancelTypeList' :CancellationReason.objects.all().order_by("order"),
-                })
+            {
+            'shipment':shipment,
+            'siList':siList,
+            'totalShipmentItemCount':getTotalShipmentItemCount(request),
+            'cancelTypeList' :CancellationReason.objects.all().order_by("order"),
+            })
 
 @login_required
 @check_permission('Sms')
@@ -270,7 +260,7 @@ def confirm_shipment(request):
     shipment.confirmed_shipment_date = datetime.datetime.strptime(request.POST['confirmedShipmentDate'], "%m/%d/%Y")
     shipment.comment = request.POST['comment']
     shipment.save()
-    
+
     return redirect('/sms/list_shipment/')
 
 @login_required
@@ -326,7 +316,7 @@ def export_shipment_csv(request):
     response['Content-Disposition'] = 'attachment; filename='+filename+'.csv'
 
     writer = csv.writer(response)
-    writer.writerow(['id_shipment_item\tshipment_id\tbarcode\tsku\tquantity\timage_url'])
+    writer.writerow(['id_shipment_item\tshipment_id\tbarcode\tsku\tquantity\timage_url\tbrand\tzidaya_name\tsize\tsupplier_color\tsku_supplier_simple\tsku_supplier_config'])
 
     for si in siList:
         isi = si.pk
@@ -346,14 +336,29 @@ def export_shipment_csv(request):
             pass
 
         #create image url for product
+        sku = si.catalog_simple.sku
         icc = si.catalog_simple.id_catalog_config
         reverse_icc = str(icc)[::-1]
         image_url = 'http://static.zidaya.com/p/-'+reverse_icc+'-1-product.jpg'
+        brand = str(si.catalog_simple.brand)
+        zname = str(si.catalog_simple.zidaya_name)
+        try:
+            size = si.catalog_simple.simplessizes.size
+        except:
+            size = "yok"
+        color = si.catalog_simple.supplier_color
+        try:
+            simple = str(si.catalog_simple.sku_supplier_simple)
+        except:
+            simple = "-"
+        try:
+            config = str(si.catalog_simple.sku_supplier_config)
+        except:
+            config = "-"
 
-        writer.writerow([str(isi)+'\t'+str(sid)+'\t'+barcode+'\t'+si.catalog_simple.sku+'\t'+str(si.quantity_ordered)+'\t'+image_url])
+        writer.writerow([str(isi)+'\t'+str(sid)+'\t'+barcode+'\t'+sku+'\t'+str(si.quantity_ordered)+'\t'+image_url+'\t'+brand+'\t'+zname+'\t'+size+'\t'+color+'\t'+simple+'\t'+config])
 
     return response
-
 
 @login_required
 @check_permission('Sms')
@@ -369,3 +374,125 @@ def add_invoice(request):
             pass
 
     return redirect('/sms/list_shipment/')
+
+@login_required
+@check_permission('Sms')
+def export_shipments_excel(request):
+    shipments = Shipment.objects.all()
+    #shipments = request.GET['slist']
+
+    book = xlwt.Workbook(encoding='utf8')
+    sheet = book.add_sheet('untitled')
+
+    field_names = ['status','number','supplier','shipment_type','is_consignment','totalShipmentItemCount','create_date','proposed_shipment_date','confirmed_shipment_date','date_received','comment','damaged_return_rate','cancel_reason','create_user','invoice_url']
+    index_counter = 1
+
+
+    for index_i,field in enumerate(field_names):
+        sheet.write(0,index_i,[unicode(field).encode('utf-8') ])
+        index_counter +=1
+
+    for index_i,a_shipment in enumerate(shipments):
+        for index_j,field in enumerate(field_names):
+            if index_j == 3 :
+                sheet.write(index_i+1,index_j,[unicode(a_shipment.shipmentType.name).encode('utf-8') ])
+            elif index_j == 12:
+                try:
+                    sheet.write(index_i+1,index_j,[unicode(a_shipment.cancel_reason.name).encode('utf-8') ])
+                except :
+                    pass
+            elif index_j == 13:
+                try:
+                    sheet.write(index_i+1,index_j,[unicode(a_shipment.create_user.email).encode('utf-8') ])
+                except :
+                    pass
+            else:
+                sheet.write(index_i+1,index_j,[unicode(getattr(a_shipment, field)).encode('utf-8') ])
+
+    response = HttpResponse(mimetype='application/vnd.ms-excel')
+
+    file_string = 'attachment; filename='+'shipment'+'.xls'
+    response['Content-Disposition'] = file_string
+
+    book.save(response)
+    return response
+
+@login_required
+@check_permission('Sms')
+def export_a_shipment_excel(request):
+    sid = request.GET.get("sid",None)
+    if sid is  None or Shipment.objects.filter(pk=sid).count() == 0:
+        return redirect("/sms/list_shipment/")
+
+    shipment = Shipment.objects.get(pk=sid)
+    siList = ShipmentItem.objects.filter(shipment=shipment)
+
+    book = xlwt.Workbook(encoding='utf8')
+    sheet = book.add_sheet('untitled')
+
+    field_names = ['quantity_ordered','sku','sku_config','cost','tax_percent','barcode_ean','barcode_to_export','brand','sku_supplier_config','sku_supplier_simple','zidaya_name','supplier_color','size','supplier_material','supplier_product_name']
+    index_counter = 1
+
+    for index_i,field in enumerate(field_names):
+        sheet.write(0,index_i,[unicode(field).encode('utf-8') ])
+        index_counter +=1
+
+    for index_i,an_item in enumerate(siList):
+        ics = CatalogSimple.objects.get(pk=an_item.catalog_simple.pk)
+        for index_j,field in enumerate(field_names):
+            if index_j == 0:
+                sheet.write(index_i+1,index_j,[unicode(getattr(an_item, field)).encode('utf-8') ])
+            elif index_j == 12:
+                sheet.write(index_i+1,index_j,[unicode(ics.simplessizes.size).encode('utf-8') ])
+            else:
+                sheet.write(index_i+1,index_j,[unicode(getattr(ics, field)).encode('utf-8') ])
+
+    response = HttpResponse(mimetype='application/vnd.ms-excel')
+
+    file_string = 'attachment; filename='+'shipment'+'.xls'
+    response['Content-Disposition'] = file_string
+
+    book.save(response)
+    return response
+
+@login_required
+@check_permission('Sms')
+def export_basket_excel(request):
+    siList = request.session.get("siList",[])
+
+    book = xlwt.Workbook(encoding='utf8')
+    sheet = book.add_sheet('untitled')
+
+    field_names = ['quantity_ordered','sku','sku_config','cost','tax_percent','barcode_ean','barcode_to_export','brand','sku_supplier_config','sku_supplier_simple','zidaya_name','supplier_color','size','supplier_material','supplier_product_name']
+    index_counter = 1
+
+    for index_i,field in enumerate(field_names):
+        sheet.write(0,index_i,[unicode(field).encode('utf-8') ])
+        index_counter +=1
+
+    for index_i,an_item in enumerate(siList):
+        ics = CatalogSimple.objects.get(pk=an_item.catalog_simple.pk)
+        for index_j,field in enumerate(field_names):
+            if index_j == 0:
+                try:
+                    sheet.write(index_i+1,index_j,[unicode(getattr(an_item, field)).encode('utf-8') ])
+                except :
+                    pass
+            elif index_j == 12:
+                try:
+                    sheet.write(index_i+1,index_j,[unicode(ics.simplessizes.size).encode('utf-8') ])
+                except :
+                    pass
+            else:
+                try:
+                    sheet.write(index_i+1,index_j,[unicode(getattr(ics, field)).encode('utf-8') ])
+                except :
+                    pass
+
+    response = HttpResponse(mimetype='application/vnd.ms-excel')
+
+    file_string = 'attachment; filename='+'shipment'+'.xls'
+    response['Content-Disposition'] = file_string
+
+    book.save(response)
+    return response
